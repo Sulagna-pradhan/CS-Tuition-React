3f444dc531c4d84bb19e8dc1b9a531a43be83e91
Sulagna Pradhan <infinityresources81@gmail.com>
2025-04-13 23:16:21 +0530

    Changes to be committed:

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -227,7 +227,7 @@ export default function Header() {
             <div className="ml-4 pl-4 border-l border-gray-200 flex items-center">
               <Link to="/login" className="group flex items-center space-x-2 bg-gradient-to-r from-indigo-600 to-blue-500 hover:from-indigo-700 hover:to-blue-600 text-white px-5 py-2 rounded-lg text-sm font-medium transition-all duration-300 shadow-md hover:shadow-lg">
                 <FontAwesomeIcon icon={faArrowRight} className="h-4 w-4" />
-                <span>Login</span>
+                <span>Register</span>
               </Link>
 
               {/* Notification Bell */}
